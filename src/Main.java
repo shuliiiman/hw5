b9e1c619d74164065676137dbccea2cc92d85d47
Alexander Shulimov <a.shulimov@yandex.ru>
2023-07-25 00:23:53 +0300

    2 commit

--- src/Main.java
+++ src/Main.java
@@ -46,24 +46,25 @@ public class Main {
     public static void task3(){
         System.out.println("Задание номер 3");
         int year = 2021;
-        if (year%4 == 0){
+        if (year%4 == 0 && year%400==0 || year%100!=0){
             System.out.println(year+" год является високосным");
         }
-        else{
+        else {
             System.out.println(year+" год не является високосным");
         }
     }
     public static void task4(){
         System.out.println("Задание номер 4");
         int deliveryDistance = 95;
+        int day=1;
         if (deliveryDistance<20){
-            System.out.println("Потребуется дней: 1");
+            System.out.println("Потребуется дней: "+ day);
         }
         else if(deliveryDistance >=20 && deliveryDistance<60){
-            System.out.println("Потребуется дней: 2");
+            System.out.println("Потребуется дней: "+ (day+1));
         }
         else if(deliveryDistance >=60 && deliveryDistance<100){
-            System.out.println("Потребуется дней: 3");
+            System.out.println("Потребуется дней: "+ (day+2));
         }
         else{
             System.out.println("Доставки нет");
@@ -72,5 +73,32 @@ public class Main {
     }
     public static void task5(){
         System.out.println("Задание номер 5");
+        int monthNumber = 12;
+        switch (monthNumber){
+            case 1:
+            case 2:
+            case 3:
+                System.out.println("Зима");
+                break;
+            case 4:
+            case 5:
+            case 6:
+                System.out.println("Весна");
+                break;
+            case 7:
+            case 8:
+            case 9:
+                System.out.println("Лето");
+                break;
+            case 10:
+            case 11:
+            case 12:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Неизвестный сезон");
+
+        }
+
     }
 }
